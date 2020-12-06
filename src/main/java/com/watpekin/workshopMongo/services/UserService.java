e66a29b711a1e1c8bb566f008e218d73145c7ac3
watpekin <henrique.watpequim@gmail.com>
2020-12-06 08:30:24 -0300

    Obtendo usuário por id

--- src/main/java/com/watpekin/workshopMongo/services/UserService.java
+++ src/main/java/com/watpekin/workshopMongo/services/UserService.java
@@ -1,12 +1,15 @@
 package com.watpekin.workshopMongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+
 import com.watpekin.workshopMongo.domain.User;
 import com.watpekin.workshopMongo.repository.UserRepository;
+import com.watpekin.workshopMongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -16,5 +19,9 @@ public class UserService {
 	
 	public List<User> findAll(){
 		return repo.findAll();
+	}
+	public User findById(String id) {
+		Optional<User> obj = repo.findById(id);
+		return obj.orElseThrow(()-> new ObjectNotFoundException(id));
 	}
 }
