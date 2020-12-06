b5602f0482df551663a15f5d839da1ff8206a9a9
watpekin <henrique.watpequim@gmail.com>
2020-12-06 10:30:25 -0300

    end point para retornar o post de um usuário

--- src/main/java/com/watpekin/workshopMongo/resources/UserResource.java
+++ src/main/java/com/watpekin/workshopMongo/resources/UserResource.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.watpekin.workshopMongo.domain.Post;
 import com.watpekin.workshopMongo.domain.User;
 import com.watpekin.workshopMongo.dto.UserDTO;
 import com.watpekin.workshopMongo.services.UserService;
@@ -64,6 +65,13 @@ public class UserResource {
 		obj.setId(id);
 		obj = service.update(obj);
 		return ResponseEntity.noContent().build();
+
+	}
+	
+	@RequestMapping(value = "/{id}/posts", method = RequestMethod.GET)
+	public ResponseEntity<List<Post>> findPosts(@PathVariable String id) {		
+		User user = service.findById(id);		
+		return ResponseEntity.ok().body(user.getPosts());
 
 	}
 }
