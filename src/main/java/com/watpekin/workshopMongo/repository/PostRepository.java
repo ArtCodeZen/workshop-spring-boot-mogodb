0d6b978efaadafc3c158188100b8c8e770626717
watpekin <henrique.watpequim@gmail.com>
2020-12-07 11:17:02 -0300

    consulta simples com @query

--- src/main/java/com/watpekin/workshopMongo/repository/PostRepository.java
+++ src/main/java/com/watpekin/workshopMongo/repository/PostRepository.java
@@ -3,6 +3,7 @@ package com.watpekin.workshopMongo.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.watpekin.workshopMongo.domain.Post;
@@ -10,4 +11,6 @@ import com.watpekin.workshopMongo.domain.Post;
 @Repository
 public interface PostRepository extends MongoRepository<Post, String>{
 	List<Post> findByTitleContainingIgnoreCase(String text);
+	@Query("{'title' : {$regex: ?0, $options: 'i'}}")
+	List<Post> searchTitle(String text);
 }
